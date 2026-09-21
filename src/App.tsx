50c9c6e6dcc4bb5bb738ad25f2d087496903761e
SHERIF NADER MORTADA ELSAYED <sherif.nader07@gmail.com>
2026-09-21 23:47:33 +0300

    fix: close notification SurfacePresence wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -915,6 +915,7 @@ export default function App() {
           </div>
         </div>
       )}
+      </SurfacePresence>
 
       {/* Main Container */}
       <main className="relative z-10 flex-1 max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8 py-5 sm:py-6 space-y-6">
